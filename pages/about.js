9d0633979d62d7e03847ff3cdd6ae433e1d9aa63
Abel Haris Harsono <pregabalin@Abels-MacBook-Pro.local>
2023-11-11 18:50:59 +0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Navbar from "@/comps/navbar";
+import Head from "next/head"
+
+const About = () => {
+    return ( 
+    <>
+    <Head>
+        <title>Ninja List | About</title>
+        <meta name = "keywords" content='ninjas'/>
+      </Head>
+    <div>
+        <h1>About</h1>
+        <p>Fugiat commodo duis elit esse. Ipsum esse consequat velit qui. Commodo quis non nulla exercitation sunt in. Eu magna esse ullamco eiusmod sit voluptate dolor et eu et ullamco proident sunt. Amet magna reprehenderit irure consectetur ad incididunt irure qui pariatur eu et sunt id incididunt. Duis excepteur sit ea adipisicing pariatur reprehenderit aliquip proident. Nostrud mollit ex non excepteur consectetur et enim sint tempor.</p>
+        <p>Eu magna proident sint velit ullamco magna enim. Ut laborum veniam laborum magna amet duis magna enim id et veniam. Esse qui deserunt in dolor nulla ullamco. Sit enim ut ullamco dolor. Exercitation ad enim ad irure.</p>
+    
+    </div> </>);
+}
+ 
+export default About;
